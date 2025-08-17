b2b9891385d4e9a5efdcef61b227179c051e3214
Kshitij Shukla <kshitijshukla264@gmail.com>
2025-08-17 22:54:28 +0530

    Fix: Use VITE_API_URL for backend connection

--- frontend/src/features/taskService.js
+++ frontend/src/features/taskService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/tasks/';
-
+const API_URL = import.meta.env.VITE_API_URL + '/api/tasks/'
 // Create new task
 const createTask = async (taskData, token) => {
   const config = { headers: { Authorization: `Bearer ${token}` } };
